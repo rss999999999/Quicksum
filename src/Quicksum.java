f2c770626655deb89222b30b4447ab90ac0f4f2e
Rodrigo <rss.999999999@gmail.com>
2023-09-19 10:44:32 -0300

    for (int i = 0; i < packet.length(); i++) if (packet.charAt(i) == ' ') if (packet.charAt(i) < 'A' || packet.charAt(i) > 'Z') sum += (i + 1) * (packet.charAt(i) - 'A' + 1);

--- src/Quicksum.java
+++ src/Quicksum.java
@@ -1,5 +1,46 @@
+/*
+For this problem, you will implement a checksum algorithm called Quicksum. A Quicksum packet allows only uppercase letters and spaces. It always begins and ends with an uppercase letter.
+
+        Otherwise, spaces and uppercase letters can occur in any combination, including consecutive spaces.
+
+        A Quicksum is the sum of the products of each character’s position in the packet times the character’s value. A space has a value of zero, while letters have a value equal to their position in the alphabet.
+
+        So, A = 1, B = 2, etc., through Z = 26. Here are example Quicksum calculations for the packets “ACM” and “A C M”:
+
+
+        ACM
+        1 × 1 + 2 × 3 + 3 × 13 = 46
+
+        A C M
+        1 x 1 + 3 x 3 + 5 * 13 = 75
+        When the packet doesn't have only uppercase letters and spaces or just spaces the result to quicksum have to be zero (0).
+
+
+        AbqTH #5 = 0
+*/
+
 public class Quicksum {
     public int quicksum(String packet){
-        //Code time :)
+        int sum = 0;
+        //This line starts a for loop that iterates over the characters in the packet string.
+        //For example, if the packet string is equal to "ABC", then the value of packet.length() would be 3.
+        for (int i = 0; i < packet.length(); i++) {
+            //This line checks if the character at index i is a space. If it is, the continue statement skips the rest of the loop and goes to the next iteration.
+            if (packet.charAt(i) == ' ') {
+                continue;
+            }
+            //This line checks if the character at index i is not an uppercase letter. If it is not, the method returns 0.
+            if (packet.charAt(i) < 'A' || packet.charAt(i) > 'Z') {
+                return 0;
+            }
+            //This line adds the product of the character’s position in the packet times the character’s value to the sum variable.
+            sum += (i + 1) * (packet.charAt(i) - 'A' + 1);
+            //For example, if the packet string is equal to "ABC" and the current character is 'B', then the quicksum of the current character would be calculated as follows:
+            //sum = (i + 1) * (packet.charAt(i) - 'A' + 1)
+            //sum = 2 * (66 - 65 + 1)
+            //sum = 2 * 2
+            //sum = 4
+        }
+        return sum;
     }
 }
--- tests/QuicksumTest.java
+++ tests/QuicksumTest.java
@@ -1,4 +1,70 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+import java.util.Random;
+
 public class QuicksumTest {
-  
+    public static final char[] VALID = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ".toCharArray();
+    public static final char[] INVALID = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\\#\\$\\%\\&\\/\\(\\)\\=\\* ".toCharArray();
+
+    public int solution(String packet){
+        int result = 0;
+        if(packet.matches("^[A-Z ]+$")){
+            char[] packetArray = packet.toCharArray();
+            for(int i = 0; i < packetArray.length; i++){
+                if((int)packetArray[i] != 32) {
+                    result += ((int)packetArray[i] - 64) * ( i + 1 );
+                }
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void testQuicksumBasic() {
+        Quicksum quicksumObject = new Quicksum();
+        assertEquals(46,quicksumObject.quicksum("ACM"));
+        assertEquals(650,quicksumObject.quicksum("MID CENTRAL"));
+        assertEquals(15,quicksumObject.quicksum("BBC"));
+        assertEquals(0,quicksumObject.quicksum("???"));
+        assertEquals(0,quicksumObject.quicksum("axg"));
+        assertEquals(0,quicksumObject.quicksum("234 234 WEF ASDF AAA 554211 ???? "));
+        assertEquals(75,quicksumObject.quicksum("A C M"));
+        assertEquals(6201,quicksumObject.quicksum("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        assertEquals(12051,quicksumObject.quicksum("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"));
+        assertEquals(848640,quicksumObject.quicksum("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"));
+        assertEquals(33,quicksumObject.quicksum("Z     A"));
+        assertEquals(0,quicksumObject.quicksum("12312 123 123 asd asd 123 $$$$/()="));
+        assertEquals(0,quicksumObject.quicksum("As"));
+        assertEquals(0,quicksumObject.quicksum("         "));
+
+    }
+
+    @Test
+    public void testQuicksumRandom() {
+        Quicksum quicksumObject = new Quicksum();
+        String packet = "";
+        for(int i = 1; i <= 100; i++){
+            packet = (i % 2 == 0) ? validInput() : invalidInput();
+            assertEquals(solution(packet),quicksumObject.quicksum(packet));
+        }
+    }
+
+    public String validInput(){
+        String text = "";
+        Random randomGenerator = new Random();
+        for(int i = 1; i <= randomGenerator.nextInt(100); i++){
+            text += VALID[randomGenerator.nextInt(VALID.length)];
+        }
+        return text;
+    }
+
+    public String invalidInput(){
+        String text = "";
+        Random randomGenerator = new Random();
+        for(int i = 1; i <= randomGenerator.nextInt(100); i++){
+            text += INVALID[randomGenerator.nextInt(INVALID.length)];
+        }
+        return text;
+    }
 }
